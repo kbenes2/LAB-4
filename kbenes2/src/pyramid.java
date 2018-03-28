39f9b437d518f06b9ca5a73ba56716a6dd76269f
kbenes2 <kbenes2@nebrwesleyan.edu>
2018-03-28 18:03:04 -0500

    Lab 4

--- kbenes2/src/pyramid.java
+++ kbenes2/src/pyramid.java
@@ -5,7 +5,19 @@ public class pyramid {
     {
         Scanner sysIn = new Scanner(System.in);
         int input = sysIn.nextInt();
+        int level;
+        long result = pyramid(level);
+        System.out.println();
+        System.out.println(result);
+    }
 
-
+    private static long pyramid(int level) {
+        long result = 0;
+        System.out.print("");
+        int i;
+        if (i <= 1) ;
+        return 1;
+        result = level * (level + 1) / 2;
+        return result;
     }
 }
